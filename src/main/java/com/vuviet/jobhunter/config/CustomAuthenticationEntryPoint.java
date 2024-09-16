e0e1c9f344d42583868ab59cec906662d0fae014
vuvip44 <vuvip2002z@gmail.com>
2024-09-16 16:37:36 +0700

    fix custom  exception

--- src/main/java/com/vuviet/jobhunter/config/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/vuviet/jobhunter/config/CustomAuthenticationEntryPoint.java
@@ -14,6 +14,7 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Optional;
 
 @Component
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint{
@@ -33,6 +34,11 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint{
 
         RestResponse<Object> res = new RestResponse<Object>();
         res.setStatusCode(HttpStatus.UNAUTHORIZED.value());
+
+        String errorMessage= Optional.ofNullable(authException.getCause())
+                        .map(Throwable::getMessage)
+                                .orElse(authException.getMessage());
+
         res.setError(authException.getCause().getMessage());
         res.setMessage("Token không hợp lệ (hết hạn, không đúng định dạng, hoặc không truyền JWT ở header)...");
 
--- src/main/java/com/vuviet/jobhunter/config/SecurityConfiguration.java
+++ src/main/java/com/vuviet/jobhunter/config/SecurityConfiguration.java
@@ -51,10 +51,10 @@ public class SecurityConfiguration {
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults())
                         .authenticationEntryPoint(customAuthenticationEntryPoint))
 
-                .exceptionHandling(
-                        exceptions -> exceptions
-                                .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint()) //401
-                                .accessDeniedHandler(new BearerTokenAccessDeniedHandler())) //403
+//                .exceptionHandling(
+//                        exceptions -> exceptions
+//                                .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint()) //401
+//                                .accessDeniedHandler(new BearerTokenAccessDeniedHandler())) //403
                 .formLogin(f->f.disable())
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
