a8688650f00fcd9adc9f9394399f572189d4fe7f
vuvip44 <vuvip2002z@gmail.com>
2024-09-16 11:29:46 +0700

    okkk

--- src/main/java/com/vuviet/jobhunter/repository/UserRepository.java
+++ src/main/java/com/vuviet/jobhunter/repository/UserRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
     User getByEmail(String email);
+    User getById(long id);
 }
